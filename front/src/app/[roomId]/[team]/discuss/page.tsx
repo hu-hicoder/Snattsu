1a199aac5d4bb141317c7b760e27219b3cc090ed
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-06-08 22:22:04 +0900

    feat(front): ディスカッションタイム

--- front/src/app/[roomId]/[team]/discuss/page.tsx
+++ front/src/app/[roomId]/[team]/discuss/page.tsx
@@ -22,11 +22,38 @@ export default function DiscussPage() {
   const members = Number(searchParams.get("members") || 1);
   const productId = Number(searchParams.get("productId"));
 
+  const [remaining, setRemaining] = useState<number>(30);
+
   useEffect(() => {
-    fetchSpecialCardList();
-  }, [router]);
+    const interval = setInterval(async () => fetchCountDown(), 1000);
+    return () => clearInterval(interval);
+  }, [remaining]);
+
+  const fetchCountDown = async () => {
+    const res = await fetch("http://localhost:8080/api/discuss-timer", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ roomId, productId }),
+    });
+
+    const json = await res.json();
+
+    if (res.ok) {
+      setRemaining(json.remaining as number);
+
+      if (json.remaining <= 0) {
+        router.push(
+          `/${roomId}/${team}/result?members=${members}&productId=${productId}`
+        );
+      }
+    }
+  };
 
   const [cards, setCards] = useState<Cards | null>(null);
+  useEffect(() => {
+    fetchSpecialCardList();
+  }, []);
+
   const fetchSpecialCardList = async () => {
     const res = await fetch("http://localhost:8080/api/special-card", {
       method: "GET",
@@ -75,32 +102,35 @@ export default function DiscussPage() {
 
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen gap-8">
-      <p className="text-lg">スペシャルカードを使用する</p>
-      <div className="flex flex-row gap-5 items-center justify-center">
-        {Object.entries(cards).map(([key, card]) => (
-          <button
-            key={key}
-            onClick={() => useSpecialCard(key)}
-            onMouseEnter={() => showDescription(card.description)}
-            onMouseLeave={() => hideDescription()}
-            style={{ marginRight: "10px" }}
-            disabled={usedCard !== false}
-            className="peer bg-green-300 rounded-xl p-1 text-xl text-gray-900 disabled:bg-green-50 disabled:text-gray-500 cursor-pointer"
-          >
-            {card.name}
-          </button>
-        ))}
+      <p>正解発表まで: {remaining}</p>
+      <div className="relative">
+        <p className="text-lg text-center pb-3">スペシャルカードを使用する</p>
+        <div className="flex flex-row gap-5 items-center justify-center">
+          {Object.entries(cards).map(([key, card]) => (
+            <button
+              key={key}
+              onClick={() => useSpecialCard(key)}
+              onMouseEnter={() => showDescription(card.description)}
+              onMouseLeave={() => hideDescription()}
+              style={{ marginRight: "10px" }}
+              disabled={usedCard !== false}
+              className="peer bg-green-300 rounded-xl p-1 text-xl text-gray-900 disabled:bg-green-50 disabled:text-gray-500 cursor-pointer"
+            >
+              {card.name}
+            </button>
+          ))}
 
-        <div
-          className={`absolute bottom-14 left-12 mt-5 p-3 bg-white border rounded shadow text-gray-600
+          <div
+            className={`absolute bottom-14 left-12 mt-5 p-3 bg-white border rounded shadow text-gray-600
                         transition-opacity duration-300 z-10 peer-hover:block hidden`}
-        >
-          <strong>説明:</strong> {hoverDescription}
+          >
+            <strong>説明:</strong> {hoverDescription}
+          </div>
         </div>
-      </div>
 
-      {usedCard === true && <p>スペシャルカード{usedCardName}を使用!</p>}
-      {usedCard === "used" && <p>既にカードは使用されています。</p>}
+        {usedCard === true && <p>スペシャルカード{usedCardName}を使用!</p>}
+        {usedCard === "used" && <p>既にカードは使用されています。</p>}
+      </div>
     </div>
   );
 }
